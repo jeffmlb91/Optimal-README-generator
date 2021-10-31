593f6ab2979c4314b87b545df89008ae2a1bb6a5
jeffmlb91 <jeffmlb91@gmail.com>
2021-10-31 09:45:13 -0400

    Add question to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,44 @@ const inquirer = require('inquirer');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'please enter a description of your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use of this application',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Is there any collaborators for this project?',
+    },
+    {
+        type: 'input',
+        name: 'licence',
+        message: 'What license was used for this README?',
+    },
+    {
+        type: 'input',
+        name: 'Test',
+        message: 'What command do you use to test this App?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
