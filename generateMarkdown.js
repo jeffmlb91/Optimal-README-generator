29d93c825ab4e3559f48798faeebd8febc1e945f
jeffmlb91 <jeffmlb91@gmail.com>
2021-10-31 14:09:40 -0400

    refactored the code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,4 +1,66 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+//TODO: Create a function that returns a license badge based on which license is passed in
+// function to generate markdown for README
+function licenseBadge(data) {
+  const licenseType = data.license[0];
+  let licenseString = " "
+  if (licenseType === "MIT") {
+    licenseString = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  };
+  if (licenseType === "GNU General Public License 2.0") {
+    licenseString = `![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)`
+  };
+  if (licenseType === "Apache License 2.0") {
+     licenseString = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
+    };
+  if (licenseType === "GNU General Public License 3.0") {
+      licenseString = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+    };
+  return licenseString
+};
+
+//Create a function to generate markdown for README
+
+function generateMarkdown(data) {
+    return `# ${data.title}
+  ## Table of Contents:
+    1. [Description](#description) 
+    2. [Installation](#Installation)
+    3. [Usage](#Usage)  
+    4. [Contributing](#Contributing)
+    5. [Tests](#Tests)
+    6. [License](#License)
+    7. [GitHub](#GitHub)
+    8. [E-mail](#E-mail)
+  ## Description
+  ${data.description} 
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ## License
+  ${licenseBadge(data)}
+  ## GitHub
+  ${data.github}
+  ## E-mail
+  ${data.email}`
+}
+  module.exports = generateMarkdown;
+
+
+
+
+
+
+
+
+
+
+
+/*// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
 
@@ -45,5 +107,5 @@ function generateMarkdown(data) {
 
 `;
 }
-
+*/
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,79 +1,165 @@
-// TODO: Include packages needed for this application
+
+function init() {
+    
+    const inquirer = require('inquirer');
+    const fs = require('fs');
+    const generate = require('./generateMarkdown');
+    const path = require('path');
+
+    inquirer
+     .prompt([
+
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'please enter a description of your project',
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'What are the steps required to install your project?',
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'Provide instructions and examples for use of this application',
+        },
+        {
+            type: 'input',
+            name: 'Credits',
+            message: 'Is there any collaborators for this project?',
+        },
+        {
+            type: 'checkbox',
+            name: 'License',
+            message: 'What license was used for this README?',
+            choices: ["MIT", "GNU Lesser General Public License v3.0", "Mozilla Public License 2.0", "Apache License 2.0", "Boost Software License 1.0", "The Unlicense"]
+        },
+        {
+            type: 'input',
+            name: 'Test',
+            message: 'What command do you use to test this App?',
+        },
+    ]).then((response) => {
+
+        return fs.writeFileSync(path.join (process.cwd(), "README.md"), generate(response));
+    });
+
+}
+init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const generatorMarkdown = require("./generateMarkdown")
-const fs = require("fs"); 
+const fs = require('fs');
 
 
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = () => {
+    return inquirer.prompt([
     
-    {
-        type: "input",
-        message: "What is your GitHub username?",
-        name: "UserName",
-    },
-    {
-        type: "input",
-        message: "What is your email address?",
-        name: "Email",
-    },
-    
-    {
-        type: "input",
-        message: "What is the title for your project?",
-        name: "Title",
-    },
-    {
-        type: "input",
-        message: "Please give description of your project.",
-        name: "Description",
-    },
-    {
-        type: "input",
-        message: "What necessary dependencies must be installed to run this app?",
-        name: "Installation",
-    },
-    {
-        type: "input",
-        message: "What is this app used for?",
-        name: "Usage",
-    },
-    {
-        type: "input",
-        message: "What license was used for this README?",
-        name: "License",
-    },
-    
-    {
-        type: "input",
-        message: "Please add contributors",
-        name: "Contributor",
-    },
-    {
-        type: "input",
-        message: "What command do you use to test this App?",
-        name: "Test",
-    }
-    
-];
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "UserName",
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "Email",
+        },
+        
+        {
+            type: "input",
+            message: "What is the title for your project?",
+            name: "Title",
+        },
+        {
+            type: "input",
+            message: "Please give description of your project.",
+            name: "Description",
+        },
+        {
+            type: "input",
+            message: "What necessary dependencies must be installed to run this app?",
+            name: "Installation",
+        },
+        {
+            type: "input",
+            message: "What is this app used for?",
+            name: "Usage",
+        },
+        {
+            type: "input",
+            message: "What license was used for this README?",
+            name: "License",
+        },
+        
+        {
+            type: "input",
+            message: "Please add contributors",
+            name: "Contributor",
+        },
+        {
+            type: "input",
+            message: "What command do you use to test this App?",
+            name: "Test",
+        }
+        
+    ]).then(() => {
+
+    })
+}
+//questions()
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile("./demo/"+fileName, data, function(err) {
+    fs.writeFile("./generatedReadme"+fileName, data, (err) => {
         if (err) {
-          return console.log(err);
+            return console.log(err);
         }
-        console.log ("Successfully wrote: " + fileName);
+        console.log('Successfully wrote: ' + fileName);
+    })
 };
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
-    .then(function(data) {
-      writeToFile("DemoREADME.md", generatorMarkdown(data));
+    .then((data) => {
+        fs.writeToFile("generatorReadme.md", generatorMarkdown(data))
     })
+   
 }
 
 // Function call to initialize app
 init();
+*/
